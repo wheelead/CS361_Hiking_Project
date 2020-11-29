02f468f1e25c006c4a5d7af2eae2363587045fa0
c-satt <56098393+c-satt@users.noreply.github.com>
2020-11-29 11:47:10 -0800

    Update sort.py
    Added if else statement to for if filter option is on

--- sort.py
+++ sort.py
@@ -1,6 +1,9 @@
 import json
 
 def sortIt(jfy, lvl, tableDict):
-    # logic to sort data
-    print("sort")
-    return tableDict
+	# logic to sort data
+	if jfy == "checked":
+		print("sort")
+	else:
+		print("NO!")
+	return tableDict
